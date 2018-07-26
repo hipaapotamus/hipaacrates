587cd374ab9e470dffae49ee1bc359697e5c964b
Miller Wilt <miller.wilt@gmail.com>
2018-07-26 13:21:16 -0400

    Add function in dockerfile to perform entire conversion process

--- hipaacrates/dockerfile.py
+++ hipaacrates/dockerfile.py
@@ -5,6 +5,22 @@ from .crate import Crate
 DEFAULT_BASE_IMAGE = "phusion/baseimage:0.10.1"
 WORKDIR_PREFIX = "/opt/services"
 
+def make(crate: Crate) -> str:
+    string = StringIO()
+    string.write(
+        make_header(crate) + "\n"
+    )
+    string.write(
+        convert_build_steps(crate) + "\n"
+    )
+    string.write(
+        include_files(crate) + "\n"
+    )
+    string.write(
+        make_service_definition(crate) + "\n"
+    )
+    return string.getvalue()
+
 def make_header(crate: Crate, baseimage: str = None) -> str:
     if baseimage is None:
         baseimage = DEFAULT_BASE_IMAGE
