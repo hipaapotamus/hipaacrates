5248d58650b3abca3a1ac3ad204248b1b0e63711
Miller Wilt <miller.wilt@gmail.com>
2018-08-02 12:20:30 -0400

    Add three BundleLoader implementations
    Add three BundleLoader implementations:
    
    - FSBundleLoader - load bundles from a specific directory
    - HTTPBundleLoader - load bundles from a specific web server endpoint
    - CachingBundleLoader - attempt to load bundles from disk, pulling from a webserver if needed

--- hipaacrates/bundles.py
+++ hipaacrates/bundles.py
@@ -2,10 +2,12 @@ from abc import ABC, abstractmethod
 from collections import OrderedDict
 import os
 
+import requests
 from typing import Dict, Iterable, List
 
 from . import crate
 
+HIPAACRATE_BUNDLES_ENDPOINT = "/bundles"
 HIPAACRATE_BUNDLES_PREFIX = "hipaacrate_bundles"
 
 class BundleLoader(ABC):
@@ -43,3 +45,71 @@ def load_dependencies(origin: crate.Crate, loader: BundleLoader) -> List[crate.C
         for td in transitive_deps:
             crates[td.name] = td
     return list(crates.values())
+
+
+class FSBundleLoader(BundleLoader):
+    def __init__(self, directory: str = HIPAACRATE_BUNDLES_PREFIX) -> None:
+        self.directory = directory
+    
+    def load(self, name: str) -> crate.Crate:
+        return crate.read_yaml(os.path.join(self.directory, name))
+
+class HTTPBundleLoader(BundleLoader):
+    def __init__(self, host: str, endpoint: str = HIPAACRATE_BUNDLES_ENDPOINT) -> None:
+        if host.endswith("/"):
+            host = host[:-1]
+        if endpoint.endswith("/"):
+            endpoint = endpoint[:-1]
+        if not endpoint.startswith("/"):
+            endpoint = "/{}".format(endpoint)
+        
+        self.host = host
+        self.endpoint = endpoint
+    
+    def load(self, name: str) -> crate.Crate:
+        r = requests.get("{}{}/{}".format(self.host, self.endpoint, name))
+        r.raise_for_status()
+        return crate.parse(r.text)
+
+class CachingBundleLoader(BundleLoader):
+    def __init__(self, host: str, endpoint: str = HIPAACRATE_BUNDLES_ENDPOINT,
+                 cache_dir: str = HIPAACRATE_BUNDLES_PREFIX) -> None:
+        self._http_loader = HTTPBundleLoader(host, endpoint)
+        self._fs_loader = FSBundleLoader(cache_dir)
+    
+    @property
+    def host(self) -> str:
+        return self._http_loader.host
+    
+    @host.setter
+    def host(self, value: str) -> None:
+        self._http_loader.host = value
+    
+    @property
+    def endpoint(self) -> str:
+        return self._http_loader.endpoint
+    
+    @endpoint.setter
+    def endpoint(self, value: str) -> None:
+        self._http_loader.endpoint = value
+    
+    @property
+    def cache_dir(self) -> str:
+        return self._fs_loader.directory
+    
+    @cache_dir.setter
+    def cache_dir(self, value: str) -> None:
+        self._fs_loader.directory = value
+    
+    def load(self, name: str) -> crate.Crate:
+        try:
+            c = self._fs_loader.load(name)
+        except FileNotFoundError:
+            pass
+        else:
+            return c
+        
+        c = self._http_loader.load(name)
+        c.to_yaml(os.path.join(self.cache_dir, c.name))
+
+        return c
--- tests/bundles_test.py
+++ tests/bundles_test.py
@@ -1,6 +1,12 @@
+import os
+
 import pytest
+import requests
+import responses
 
 from hipaacrates import bundles, crate
+
+HERE = os.path.abspath(os.path.dirname(__file__))
 
 class MockBundleLoader(bundles.BundleLoader):
     def load(self, name):
@@ -84,3 +90,73 @@ def test_resolve_dependencies_circular(crate_obj):
 
     with pytest.raises(ValueError):
         bundles.resolve_dependencies(deps)
+
+def test_fs_bundle_loader():
+    fs_loader = bundles.FSBundleLoader(os.path.join(HERE, "fixtures"))
+    c = fs_loader.load("foo")
+    assert c.name == "foo"
+    assert c.version == "0.0.1"
+    assert c.author == "me"
+    assert c.includes == ["myapp/"]
+    assert c.build_steps == ["make", "make install"]
+    assert c.bundles == ["bar", "baz"]
+
+def test_fs_bundle_loader_no_file():
+    fs_loader = bundles.FSBundleLoader(os.path.join(HERE, "fixtures"))
+    with pytest.raises(FileNotFoundError):
+        fs_loader.load("nonexistant.bundle")
+
+@responses.activate
+def test_http_bundle_loader(crate_obj):
+    host = "http://github.com/hipaapotamus/hipaadrome"
+    responses.add(responses.GET, "{}/bundles/{}".format(host, crate_obj.name), body=crate_obj.to_yaml())
+
+    http_loader = bundles.HTTPBundleLoader(host)
+    c = http_loader.load(crate_obj.name)
+    assert c == crate_obj
+
+@responses.activate
+def test_http_bundle_loader_404(crate_obj):
+    host = "http://github.com/hipaapotamus/hipaadrome"
+    responses.add(responses.GET, "{}/bundles/{}".format(host, crate_obj.name),
+                  body="Not Found", status=404)
+
+    http_loader = bundles.HTTPBundleLoader(host)
+    with pytest.raises(requests.exceptions.HTTPError):
+        http_loader.load(crate_obj.name)
+
+@responses.activate
+def test_caching_bundle_loader_on_disk():
+    host = "http://github.com/hipaapotamus/hipaadrome"
+
+    loader = bundles.CachingBundleLoader(host, cache_dir=os.path.join(HERE, "fixtures"))
+    c = loader.load("foo")
+    assert c.name == "foo"
+    assert c.version == "0.0.1"
+    assert c.author == "me"
+    assert c.includes == ["myapp/"]
+    assert c.build_steps == ["make", "make install"]
+    assert c.bundles == ["bar", "baz"]
+
+@responses.activate
+def test_caching_bundle_loader_miss(crate_obj):
+    host = "http://github.com/hipaapotamus/hipaadrome"
+    responses.add(responses.GET, "{}/bundles/{}".format(host, crate_obj.name), body=crate_obj.to_yaml())
+
+    loader = bundles.CachingBundleLoader(host, cache_dir=os.path.join(HERE, "fixtures"))
+    c = loader.load(crate_obj.name)
+    try:
+        assert c == crate_obj
+        assert os.path.isfile(os.path.join(HERE, "fixtures", crate_obj.name))
+    finally:
+        os.remove(os.path.join(HERE, "fixtures", crate_obj.name))
+    
+
+@responses.activate
+def test_caching_bundle_loader_404(crate_obj):
+    host = "http://github.com/hipaapotamus/hipaadrome"
+    responses.add(responses.GET, "{}/bundles/{}".format(host, crate_obj.name), body="Not Found", status=404)
+
+    loader = bundles.CachingBundleLoader(host, cache_dir=os.path.join(HERE, "fixtures"))
+    with pytest.raises(requests.exceptions.HTTPError):
+        loader.load(crate_obj.name)
